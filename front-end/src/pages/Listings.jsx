54c08436c0b52a3f3b742413a75726d06f77c9fa
Aymen <aymenajinou2004@gmail.com>
2024-09-18 03:17:34 +0400

    static changes

--- front-end/src/pages/Listings.jsx
+++ front-end/src/pages/Listings.jsx
@@ -11,7 +11,7 @@ const Listings = () => {
     <div>
       {/* <NavBar isLoggedIn={false} activeTab="listings" /> */}
       
-      <div className="relative flex flex-col justify-center items-center h-lvh overflow-hidden">
+      <div className="relative flex flex-col justify-center items-center min-h-screen overflow-hidden">
         {/* Blurred Background Image */}
         <div
           className="absolute inset-0 bg-cover bg-center"
@@ -23,21 +23,21 @@ const Listings = () => {
         ></div>
 
         {/* Overlay to darken the background image */}
-        <div className="absolute inset-0 bg-gray-950/20"></div>
+        <div className="absolute inset-0 bg-gray-950/30"></div>
 
         {/* Content */}
-        <div className="relative flex lg:flex-row flex-col justify-center items-center w-4/6 m-10 z-10">
-          <Link className="flex flex-col justify-center items-center h-96 w-96 bg-[#F7E7DC]/30 hover:bg-[#F7E7DC]/90 rounded-3xl shadow-2xl m-5 transition-colors" to="/listview">
-          <img className="h-72 w-auto" src={listImage} alt="listview" />
-          <h1 className="text-[30px]">List View</h1>
+        <div className="relative flex flex-col lg:flex-row justify-center items-center w-full lg:w-4/6 mx-4 lg:mx-10 z-10">
+          <Link className="flex flex-col justify-center items-center h-80 w-full lg:w-80 bg-[#F7E7DC]/30 hover:bg-[#F7E7DC]/90 rounded-3xl shadow-2xl m-4 lg:m-5 transition-colors" to="/listview">
+            <img className="h-48 w-auto" src={listImage} alt="listview" />
+            <h1 className="text-xl lg:text-2xl mt-2">List View</h1>
           </Link>
-          <Link className="flex flex-col justify-center items-center h-96 w-96 bg-[#F7E7DC]/30 hover:bg-[#F7E7DC]/90 rounded-3xl shadow-2xl m-5 transition-colors" to="/mapview">
-          <img className="h-72 w-auto" src={mapImage} alt="mapview" />
-          <h1 className="text-[30px]">Map View</h1>
+          <Link className="flex flex-col justify-center items-center h-80 w-full lg:w-80 bg-[#F7E7DC]/30 hover:bg-[#F7E7DC]/90 rounded-3xl shadow-2xl m-4 lg:m-5 transition-colors" to="/mapview">
+            <img className="h-48 w-auto" src={mapImage} alt="mapview" />
+            <h1 className="text-xl lg:text-2xl mt-2">Map View</h1>
           </Link>
         </div>
 
-        <div className="2xl:block w-1/6 hidden"></div>
+        <div className="w-full lg:w-1/6 hidden lg:block"></div>
       </div>
 
       <Footer />
